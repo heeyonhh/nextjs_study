ea038432abb4b307c7bb3b6ff00148210c7afaf3
heeyonhh <lightram23@gmail.com>
2023-08-05 20:29:36 +0900

    css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import './globals.css'
 
 export const metadata = {
@@ -8,7 +9,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <h1><a href='/'>WEB</a></h1>
+        <ol>
+          <li><Link href='/read/1'>html</Link></li>
+          <li><Link href='/read/2'>css</Link></li>
+        </ol>
+        {children}
+          <ul>
+            <li><Link href='/create'>Create</Link></li>
+            <li><Link href='/update/1'>Update</Link></li>
+            <li><input type='button' value='delete' /></li>
+          </ul>
+      </body>
     </html>
   )
 }
